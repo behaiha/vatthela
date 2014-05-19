855b235393d24f66efbf997ff30cc1e044f60605
tiennm54 <minhtienuet@gmail.com>
2014-05-19 16:02:01 +0700

    CHO NHA MAY

--- protected/components/Body_Header.php
+++ protected/components/Body_Header.php
@@ -1,8 +1,7 @@
 <?php
 class Body_Header extends CWidget{
     public function init(){
-        $model = Slideshow::model()->findAll();
-        $this->render('body_header',array('model'=>$model));
+        $this->render('body_header');
     }
 }
 ?>
