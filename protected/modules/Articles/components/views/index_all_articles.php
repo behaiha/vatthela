a9ff0eebb96a7e6dc15a5f79fdbec5a6a1b7d1bf
Tuấn anh <anhntk54@gmail.com>
2014-05-19 10:49:13 +0700

    change js

--- protected/modules/Articles/components/views/index_all_articles.php
+++ protected/modules/Articles/components/views/index_all_articles.php
@@ -7,30 +7,35 @@
 
 </div><!-- ENDS Filter container -->
 <style type="text/css">
-	#filter-container{
+  #filter-container{
 
-	}
+  }
 </style>
 <script type="text/javascript">
-	$(document).ready(function() {
-		
-		$(window).scroll(function(){
-			
-			if  ($(window).scrollTop()  >=  $(document).height() - $("footer").height() - $(window).height()){
-				console.log('a');
-			    $.ajax({
+  $(document).ready(function() {
+    $(window).scroll(function(){
+      
+      if  ($(window).scrollTop()  >=  $(document).height() - $("footer").height() - $(window).height()){
+        console.log('a');
+          $.ajax({
                   type: "POST",
                   url: '<?php echo Yii::app()->createUrl("/Articles/default/load"); ?>',
-                  // data: {stt:$('#hiddenaddA').val()},
+                  // data: {stt:$('#hiddenaddA').val(),
                   // cache: false,
                   success: function(data){
-                  	// count = parseInt($('#hiddenaddA').val()) +  1;
-                    // $('#hiddenaddA').val(count);
-                    // $('#filter-container').append('<div style="clear:both;"></div>');
-                    $('#filter-container').append(data);
-                  },
-          		});
-			}
-		}); 
-	});
+                    var items = [];
+                    $.each(data, function(index, value) {
+                      d=document.createElement('figure');
+                      console.log(value);   
+                      $(d).addClass('cate3');
+                      str = '<a href="project.html" class="thumb"><img src="'+value.image+'" alt="alt"></a><figcaption><a href="project.html"><h3 class="heading">'+value.title+'</h3></a>'+value.short_description+'</figcaption>';
+                      d.innerHTML =str;
+                      items.push(d);
+                    });
+                    $('#filter-container').append($(items)).isotope( 'appended', $(items) );
+                  }
+              });
+      }
+    }); 
+  });
 </script>
--- protected/modules/Articles/controllers/DefaultController.php
+++ protected/modules/Articles/controllers/DefaultController.php
@@ -7,11 +7,23 @@ class DefaultController extends Controller
 {
 	public function actionLoad()
 	{
+		header('Access-Control-Allow-Origin: *');
+		header('Content-type: application/json');
 		$criteria = new CDbCriteria;
     	$criteria->join = "inner join category_relation cr on t.id = cr.table_id inner join categories ca on cr.category_id = ca.id";
     	$criteria->condition ="ca.type ='A'";
     	$criteria->limit = 1;
     	$model = Articles::model()->findAll($criteria);
-        $this->renderPartial('load',array('model'=>$model));
+    	$arrs = array();
+    	foreach ($model as $key => $value) {
+    		$image = Yii::app()->theme->baseUrl.'/../'.$value->path.'thumbai_300/'.$value->image;
+    		$arrs[] = array(
+    			'title'=>$value->title,
+    			'short_description'=>$value->short_description,
+    			'image'=>$image,
+    		);
+    	}
+    	echo CJSON::encode($arrs);
+        // $this->renderPartial('load',array('model'=>$model));
 	}
 }
