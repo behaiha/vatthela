37c19a1726df7d757326fa16cac538081ce2f21a
tiennm54 <minhtienuet@gmail.com>
2014-06-09 19:13:58 +0700

    abc

--- protected/modules/Articles/components/Articles_MostView.php
+++ protected/modules/Articles/components/Articles_MostView.php
@@ -3,7 +3,7 @@ class Articles_MostView extends CWidget{
     public function init(){
         $criteria = new CDbCriteria;
         $criteria->order='view DESC';
-        $criteria->limit=3;
+        $criteria->limit=5;
         $model = Articles::model()->findAll($criteria);
         $this->render('articles_mostview',array('model'=>$model));
     }
