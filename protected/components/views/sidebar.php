2a02b644cb9e1b5917b5e7cc3ea10ec304f4076f
tiennm54 <minhtienuet@gmail.com>
2014-06-24 18:41:44 +0700

    slide edit

--- protected/components/views/sidebar.php
+++ protected/components/views/sidebar.php
@@ -8,105 +8,15 @@
     
     <?php $this->widget('Videos.Components.Videos_MostView');?>
 	
-	
-	<!--<div id="post-pic-widget" class="widget photos-widget prl-panel">	
-	<h5 class="prl-block-title">Article photos</h5>
-		<div class="pt-wrapper">
-			<div class="">
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/1.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/2.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/3.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/4.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/5.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-				<a href="single.php" ><img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/images/_small/6.jpg" alt="Lorem ipsum dolor sit amet" /></a>
-			</div>
-		</div>
-	</div>	-->
-	
-	<div id="social-widget-1" class="widget social-widget prl-panel">		<!-- BEGIN WIDGET -->
-		<h5 class="prl-block-title amber">Social Network</h5>
-		<p>Were this world an endless plain, and by sailing eastward we could for ever reach new distances</p>
-		<div class="sw-wrapper">
-			<div class="sw-inner prl-clearfix">
-				<a href="#" class="fa fa-facebook" title="Facebook" data-prl-tooltip></a>
-				<a href="https://twitter.com/#" class="fa fa-twitter" title="Twitter" data-prl-tooltip></a>
-				<a href="#" class="fa fa-rss" title="RSS Feed" data-prl-tooltip></a>
-				<a href="http://www.flickr.com/photos/#" class="fa fa-flickr" title="Flickr" data-prl-tooltip></a>
-				<a href="http://www.youtube.com/#" class="fa fa-youtube" title="Youtube" data-prl-tooltip></a>
-				<a href="http://vimeo.com/#" class="fa fa-vimeo-square" title="Vimeo" data-prl-tooltip></a>
-				<a href="#" class="fa fa-linkedin" title="Linkedin" data-prl-tooltip></a>
-				<a href="#" class="fa fa-google-plus" title="Google plus" data-prl-tooltip></a>
-				<a href="http://pinterest.com/#" class="fa fa-pinterest" title="Pinterest" data-prl-tooltip></a>
-				<a href="http://instagram.com/#" class="fa fa-instagram" title="Instagram" data-prl-tooltip></a>
-			</div>
-		</div>		
-	</div>    
-	<div id="tag_cloud-2" class="widget widget_tag_cloud prl-panel"><h5 class="prl-block-title">Tags</h5>
-		<div class="tagcloud"><a href='http://www.presslayer.com/?tag=artwork' class='tag-link-6' title='2 topics' style='font-size: 16.4pt;'>artwork</a>
-			<a href='http://www.presslayer.com/?tag=photo' class='tag-link-7' title='2 topics' style='font-size: 16.4pt;'>Photo</a>
-			<a href='http://www.presslayer.com/?tag=video' class='tag-link-9' title='3 topics' style='font-size: 22pt;'>Video</a>
-			<a href='http://www.presslayer.com/?tag=videos' class='tag-link-4' title='1 topic' style='font-size: 8pt;'>Videos</a>
-			<a href='http://www.presslayer.com/?tag=videos' class='tag-link-3' title='1 topic' style='font-size: 8pt;'>Music</a>
-			<a href='http://www.presslayer.com/?tag=videos' class='tag-link-3' title='1 topic' style='font-size: 8pt;'>HTML5</a>
-			<a href='http://www.presslayer.com/?tag=videos' class='tag-link-3' title='1 topic' style='font-size: 8pt;'>CSS3</a>
-			<a href='http://www.presslayer.com/?tag=videos' class='tag-link-3' title='1 topic' style='font-size: 8pt;'>Web Design</a>
-		</div>
-	</div>
-	<div id="ins-widget-4" class="widget photos-widget prl-panel">	
-		<h5 class="prl-block-title">Instagram</h5>
-		<script>
-		$(function () {
-			$("#instagram").jqinstapics({
-			  "user_id": "294495709",
-			  "access_token": "294495709.5b9e1e6.8b935342cb424bcbb14d38b0cacebe3b",
-			  "count": 6
-			});
-		});
-		</script>
-		<div class="pt-wrapper">
-			<div id="instagram" class="pt-inner prl-clearfix"></div>
-		</div>
-	</div>		
 	<div id="search-3" class="widget widget_search prl-panel">
 		<h5 class="prl-block-title">Search</h5>
-		<form role="search" method="get" id="searchform" action="#" class="prl-form" >
+		<form role="search" method="post" id="searchform" action="<?php echo Yii::app()->createUrl('/Articles/search/rsearch');?>" class="prl-form" >
 			<div><label class="screen-reader-text" for="s">Search for:</label>
-			<input type="text" value="" name="s" id="s" />
+			<input type="text" name="keyword" id="s" />
 			<input type="submit" id="searchsubmit" value="Search" />
 			</div>
 		</form>
-	</div>	
-	<div id="nav_menu-2" class="widget widget_nav_menu prl-panel"><h5 class="prl-block-title">Custom menu</h5>
-		<div class="menu-navigation-container">
-			<ul id="menu-navigation" class="menu"><li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-1129"><a href="http://www.presslayer.com/"><i class="fa fa-home"></i> Homepage</a></li>
-				<li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-1134"><a href="http://www.presslayer.com/?cat=3"><i class="fa  fa-flask"></i> Photography</a></li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1131"><a href="#"><i class="fa fa-heart"></i> Heart</a>
-
-				</li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1132"><a href="#"><i class="fa fa-picture-o"></i> Photo</a></li>
-				<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-1133"><a href="#"><i class="fa fa-envelope"></i> Contact us</a></li>
-			</ul>
-		</div>
 	</div>
-	
-	<div id="categories-3" class="widget widget_categories prl-panel"><h5 class="prl-block-title">Categories</h5><ul>
-		<li class="cat-item cat-item-2"><a href="http://www.presslayer.com/?cat=2" title="View all posts filed under Design">Design</a>
-	</li>
-		<li class="cat-item cat-item-3"><a href="http://www.presslayer.com/?cat=3" title="View all posts filed under Photography">Photography</a>
-	</li>
-		<li class="cat-item cat-item-12"><a href="http://www.presslayer.com/?cat=12" title="View all posts filed under Slider">Slider</a>
-	</li>
-		<li class="cat-item cat-item-4"><a href="http://www.presslayer.com/?cat=4" title="View all posts filed under Videos">Videos</a>
-	</li>
-		<li class="cat-item cat-item-5"><a href="http://www.presslayer.com/?cat=5" title="View all posts filed under Wordpress">Wordpress</a>
-	</li>
-	<li class="cat-item cat-item-12"><a href="http://www.presslayer.com/?cat=12" title="View all posts filed under Slider">Slider</a>
-	</li>
-
-		
-			</ul>
-	</div>
-
 
 
 	<div id="facebook-like-widget-2" class="widget facebook-widget prl-panel"><h5 class="prl-block-title">Facebook</h5>
@@ -117,46 +27,4 @@
 		</div>
 		</div>
 	</div>
-	
-
-
-	<!--<div id="archives-3" class="widget widget-weather prl-panel">
-	<h5 class="prl-block-title">Weather</h5>
-		<div class="wt_wrapper">
-			<div class="today">
-				<div class="clearfix">
-					<div class="left">
-						<h3>TODAY</h3>
-						<p class="degree">32<i class="wi-degrees"></i></p>
-						<p><strong>Kuala Lumpur</strong></p>
-					</div>
-					<div class="right"><div class="today_icon"><i class="wi-day-lightning"></i></div></div>
-				</div>
-				<ul class="info_list">
-					<li><strong><span>Overcast</span></strong> <i class="wi-thermometer"></i> 31<i class="wi-degrees"></i> / 34<i class="wi-degrees"></i></li>
-					<li><span><i class="fa fa-umbrella"></i> 49%</span> <span><i class="wi-strong-wind"></i> 12.87km/h </span><i class="fa fa-compass"></i> NW</li>
-				</ul>
-			</div>	
-			<hr class="prl-divider">
-			<div class="next_days clearfix">
-				<div class="next_days_item">
-					<p><strong>FRI</strong></p>
-					<p><i class="wi-day-sunny-overcast"></i></p>
-					30<i class="wi-degrees"></i> / 35<i class="wi-degrees"></i>
-				</div>
-				<div class="next_days_item">
-					<p><strong>SAT</strong></p>
-					<p><i class="wi-lightning"></i></p>
-					30<i class="wi-degrees"></i> / 35<i class="wi-degrees"></i>
-				</div>
-				<div class="next_days_item last">
-					<p><strong>SUN</strong></p>
-					<p><i class="wi-cloudy"></i></p>
-					30<i class="wi-degrees"></i> / 35<i class="wi-degrees"></i>
-				</div>
-			
-			</div>
-			
-		</div>    
-	</div> -->    
 </aside>
